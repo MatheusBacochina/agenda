ae81bee0ca080ff8a75c17ed6b9e1d5c4a18e22d
MatheusBacochina <bacochinayt@hotmail.com>
2022-09-17 20:57:56 -0300

    first commit

--- renderer.js
+++ renderer.js
@@ -1,7 +1,8 @@
-/**
- * This file is loaded via the <script> tag in the index.html file and will
- * be executed in the renderer process for that window. No Node.js APIs are
- * available in this process because `nodeIntegration` is turned off and
- * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
- * to expose Node.js functionality from the main process.
- */
+const activer = document.querySelector(".a");
+activer.addEventListener("mouseleave", function () {
+  window.resizeTo(100, 100);
+});
+
+activer.addEventListener("mouseenter", function () {
+  window.resizeTo(1366, 60);
+});
